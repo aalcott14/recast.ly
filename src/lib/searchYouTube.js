ad86fc4d552a68ac5802acd3fbba62fcc8c89cbb
aalcott14 <aalcott14@gmail.com>
2017-04-07 16:46:57 -0500

    make api call work

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,21 @@
 var searchYouTube = (options, callback) => {
-  // TODO
-};
+  var searchEndpoint = 'https://www.googleapis.com/youtube/v3/search';
+  
+  callback = callback || function(d){console.log(d);};
+  options = options || {query: 'kitty', key: YOUTUBE_API_KEY, max: 5};
 
+  $.ajax({
+    url: searchEndpoint,
+    data: {
+      key: YOUTUBE_API_KEY,
+      q: options.query,
+      part: 'snippet',
+      maxResults: options.max
+    },
+    type: 'GET',
+    dataType: 'json',
+    success: callback,
+    error: function(){console.log('error');}
+  });
+};
 window.searchYouTube = searchYouTube;
